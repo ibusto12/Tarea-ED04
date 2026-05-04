4d5c3421513ebb61870b05c0eebe855d0279243b
ibusto12 <ibusto12@gmail.com>
2026-05-04 22:40:43 +0200

    Punto 6: Añadir parametro cantidad a operativa_cuenta
    Se añade el parametro cantidad de tipo float y se usa al ingresar dinero en la cuenta.

--- cuentas/Main.java
+++ cuentas/Main.java
@@ -6,10 +6,10 @@ public class Main {
         CCuenta cuenta1;
 
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 695);
     }
 
-    public static void operativa_cuenta(CCuenta cuenta1) {
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         double saldoActual;
 
         saldoActual = cuenta1.estado();
@@ -23,7 +23,7 @@ public class Main {
 
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
