bd47cea5b76111b177dd7a83c2ff3b11d4a87e39
ibusto12 <ibusto12@gmail.com>
2026-05-18 20:02:36 +0200

    Punto 7: documentar CCuenta con JavaDoc
    Se añaden comentarios JavaDoc a la clase, atributos, constructores y metodos de CCuenta.

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,15 +1,45 @@
 package cuentas;
 
+/**
+ * Representa una cuenta bancaria con un titular, un numero de cuenta, un saldo y un tipo de interes.
+ */
 public class CCuenta {
 
+    /**
+     * Nombre del titular.
+     */
     private String nombre;
+
+    /**
+     * Numero identificador de la cuenta.
+     */
     private String cuenta;
+
+     /**
+      * Saldo actual.
+      */
     private double saldo;
+
+     /**
+      * Tipo de interes de la cuenta.
+      */
     private double tipoInterés;
 
+     /**
+      * Constructor sin parametros.
+      */
     public CCuenta()
     {
     }
+
+    /**
+     * Constructor con parametros para crear una cuenta bancaria.
+     *
+     * @param nom nombre del titular de la cuenta
+     * @param cue numero de cuenta
+     * @param sal saldo inicial de la cuenta
+     * @param tipo tipo de interes de la cuenta
+     */
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
@@ -19,43 +49,94 @@ public class CCuenta {
         tipoInterés = tipo;
     }
 
+    /**
+     * Obtiene el nombre del titular.
+     *
+     * @return nombre del titular
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Modifica el nombre del titular.
+     *
+     * @param nombre nuevo nombre del titular
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el numero de cuenta.
+     *
+     * @return numero de cuenta
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Modifica el numero de cuenta.
+     *
+     * @param cuenta nuevo numero de cuenta
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * Obtiene el saldo actual.
+     *
+     * @return saldo actual de la cuenta
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Modifica el saldo de la cuenta.
+     *
+     * @param saldo nuevo saldo de la cuenta
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     * Obtiene el tipo de interes.
+     *
+     * @return tipo de interes de la cuenta
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    /**
+     * Modifica el tipo de interes.
+     *
+     * @param tipoInterés nuevo tipo de interes
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     *
+     * @return saldo actual
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Ingresa una cantidad en la cuenta.
+     *
+     * @param cantidad cantidad que se desea ingresar
+     * @throws Exception si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad < 0)
@@ -63,6 +144,12 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Retira una cantidad de la cuenta.
+     *
+     * @param cantidad cantidad que se desea retirar
+     * @throws Exception si la cantidad es negativa o si no hay saldo suficiente
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
